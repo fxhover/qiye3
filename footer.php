6a5bb24775b09b34cda10b87e9944cdc578836db
fxhover <736698959@qq.com>
2016-11-09 10:12:51 +0800

    footer font color fix

--- footer.php
+++ footer.php
@@ -4,12 +4,12 @@
 <div id="backtotop" style="display:none;visibility: visible; position: fixed; bottom: 110px;"><div id="totop-box" class="top_curr"></div></div>
 
 
-<div id="footer" style="clear:left;color:#c3c3c3;" align="center">
+<div id="footer" style="clear:left;" align="center">
 	<div id="contact">
-		<p>地址：<?php echo $config['address'];?>&nbsp;&nbsp;联系电话：<?php echo $config['tel'];?></p>
+		<p style="color:#c3c3c3;">地址：<?php echo $config['address'];?>&nbsp;&nbsp;联系电话：<?php echo $config['tel'];?></p>
   	</div>
 	<div id="copyright" align="center">
-		<p>Copyright &copy; 2016 <?php echo $config['company'];?> All Rights Reserved</p>
+		<p style="color:#c3c3c3;">Copyright &copy; 2016 <?php echo $config['company'];?> All Rights Reserved</p>
 	</div>
 	<div class="clear"></div>
 </div>
