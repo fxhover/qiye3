ddbf7d63325ec65ec576faaa62e3f70502879426
fxhover <736698959@qq.com>
2016-11-09 10:07:16 +0800

    footer change

--- footer.php
+++ footer.php
@@ -4,11 +4,13 @@
 <div id="backtotop" style="display:none;visibility: visible; position: fixed; bottom: 110px;"><div id="totop-box" class="top_curr"></div></div>
 
 
-<div id="footer" style="clear:left;">
+<div id="footer" style="clear:left;color:#c3c3c3;" align="center">
 	<div id="contact">
-		
+		<p>地址：<?php echo $config['address'];?>&nbsp;&nbsp;联系电话：<?php echo $config['tel'];?></p>
   	</div>
-	<div id="copyright" align="center">Copyright &copy; 2016 <?php echo $config['name'];?> All Rights Reserved</div>
+	<div id="copyright" align="center">
+		<p>Copyright &copy; 2016 <?php echo $config['company'];?> All Rights Reserved</p>
+	</div>
 	<div class="clear"></div>
 </div>
 </body>
